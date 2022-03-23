784009a61408e3a1a077d8ea182221def29cbde6
Oluwatobi Akinlade <oluwatobiakinlade@Oluwatobis-MBP.lan>
2022-03-23 15:10:54 +0000

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        officiis facilis dicta, consequatur fugiat velit praesentium maxime
+        quisquam tempora. Similique distinctio voluptas nulla quas voluptate
+        earum tempore corporis reiciendis totam vero quam, et aliquid modi?
+        Porro nisi repudiandae necessitatibus ad.
+      </p>
+    </section>
+  );
+};
+
+export default About;
